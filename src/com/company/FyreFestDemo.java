dc133664a42f095b6dd672d60d67c605b4bf6ca6
NM17 <nada.mahmoud@detroitlabs.com>
2019-02-01 14:54:20 -0500

    creating menu options

--- src/com/company/FyreFestDemo.java
+++ src/com/company/FyreFestDemo.java
@@ -1,8 +1,39 @@
 package com.company;
 
-public class FyreFestDemo {
+import java.util.Scanner;
 
+public class FyreFestDemo {
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner numScanner = new Scanner(System.in);
+        Scanner textScanner = new Scanner(System.in);
+        int userIntInput1;
+        String userTextInput;
+        FyreAttendant attendant = new FyreAttendant();
+
+        System.out.println("Welcome to Frye Fest Registration bruuuuuuh. Choose from the following option: ");
+        System.out.println("1. Current Registrant " +
+                "2. New Registrant" +
+                "3. Admin Access ");
+
+        userIntInput1 = numScanner.nextInt();
+
+        switch(userIntInput1) {
+
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+
+
+                default:
+
+
+
+        }
+
     }
 }
