589df9dafe8d11915d34fb3e3db5fe9050c5a453
NM17 <nada.mahmoud@detroitlabs.com>
2019-02-01 14:45:42 -0500

    Attendent class, and print details

--- src/com/company/FyreAttendant.java
+++ src/com/company/FyreAttendant.java
@@ -1,4 +1,63 @@
 package com.company;
 
 public class FyreAttendant {
+
+    private String name;
+    private String address;
+    private String email;
+    private String arrivalDate;
+    private String specialRequests;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(String arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getSpecialRequests() {
+        return specialRequests;
+    }
+
+    public void setSpecialRequests(String specialRequests) {
+        this.specialRequests = specialRequests;
+    }
+
+    public void printDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Email: " + email);
+        System.out.println("Arrival Date: " + arrivalDate);
+        System.out.println("Special Request: " + specialRequests);
+    }
+
+
+
+
+
 }
